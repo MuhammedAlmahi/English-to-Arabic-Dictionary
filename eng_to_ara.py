ed3cfeb9cbf9a9be6ed9c8559a11a2a2914f1b19
MuhammedAlmahi <144683865+MuhammedAlmahi@users.noreply.github.com>
2023-10-12 20:09:30 +0300

    Add files via upload

---
+++ eng_to_ara.py
@@ -0,0 +1,10 @@
+from D import dictionary
+
+
+dictionary = dictionary.eng_to_ara
+while True:
+    word = input("Word: ").strip()
+    if word in dictionary:
+        print(f"\nFound:\n meaning: {dictionary[word]}\n")
+    else:
+        print(f"Can not find result for '{word}' \n")
